9ca5a660ce2314d0a9de958527d98f38560701f2
k21971 <keith.simpson1971@gmail.com>
2021-04-19 03:24:21 +0000

    Fix: version compatability.

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -51,7 +51,7 @@
  * PP = patch level, ee = edit level, L = literal suffix "L",
  * with all four numbers specified as two hexadecimal digits.
  */
-#define VERSION_COMPATIBILITY 0x03070000L
+#define VERSION_COMPATIBILITY 0x01000000L
 
 /****************************************************************************/
 /* Version 3.7.x */
