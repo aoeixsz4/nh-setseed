37e7e89a0b07bb98f9c6b923830ba4eb106327e0
k21971 <keith.simpson1971@gmail.com>
2021-05-16 22:50:51 +0000

    Fix: inprogress for current version of setseed.

--- install-to-chroot.sh
+++ install-to-chroot.sh
@@ -51,8 +51,8 @@ set -e
 umask 022
 
 echo "Creating inprogress and extrainfo directories"
-mkdir -p "$NAO_CHROOT/dgldir/inprogress-seed.32"
-chown "$USRGRP" "$NAO_CHROOT/dgldir/inprogress-seed.32"
+mkdir -p "$NAO_CHROOT/dgldir/inprogress-seed.32-1"
+chown "$USRGRP" "$NAO_CHROOT/dgldir/inprogress-seed.32-1"
 mkdir -p "$NAO_CHROOT/dgldir/extrainfo-seed"
 chown "$USRGRP" "$NAO_CHROOT/dgldir/extrainfo-seed"
 
